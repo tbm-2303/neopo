68c2920f24aa879842e4e81134bb33d0d5f19440
timmy <kris>
2023-03-20 19:11:13 +0100

    .

--- src/main/java/rest/RenameMeResource.java
+++ src/main/java/rest/RenameMeResource.java
@@ -21,7 +21,7 @@ public class RenameMeResource {
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
     private static final FacadeExample FACADE = FacadeExample.getFacadeExample(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final PersonFacade personfacade = PersonFacade.getPersonFacade(EMF);
+
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
@@ -69,24 +69,5 @@ public class RenameMeResource {
         return Response.ok().entity(update).build();
     }
 
-
-    @GET
-    @Path("person{id}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public String getPersonById(@PathParam("id") int id) {
-        PersonDTO personDTO = personfacade.getById(id);
-        return GSON.toJson(personDTO);
-    }
-
-    @POST
-    @Path("addperson")
-    @Produces({MediaType.APPLICATION_JSON})
-    @Consumes({MediaType.APPLICATION_JSON})
-    public Response createPerson(String input) {
-        PersonDTO personDTO = GSON.fromJson(input, PersonDTO.class);
-        System.out.println(personDTO);
-        personDTO = personfacade.create(personDTO);
-        return Response.ok().entity(personDTO).build();
-    }
 
 }
